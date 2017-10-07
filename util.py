5df7ca80da12513d2be7a99ed55d8b3904b14252
WayneLi <p.li@berkeley.edu>
2017-10-06 23:37:35 -0700

    util added

---
+++ util.py
@@ -0,0 +1,15 @@
+import requests
+import random
+
+
+def randomWordList():
+	word_site = "http://svnweb.freebsd.org/csrg/share/dict/words?view=co&content-type=text/plain"
+	response = requests.get(word_site)
+	WORDS = response.content.splitlines()
+	return WORDS
+
+def randomWord():
+	return random.choice(randomWordList())
+
+
+
