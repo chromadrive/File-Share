baca582f88006003c17d17a07951611d53405b10
Xiaocheng Yang (Mesut) <mesut.yang97@gmail.com>
2017-10-07 20:33:29 -0700

    aligned room.py

--- room.py
+++ room.py
@@ -2,37 +2,37 @@ import util
 from client import Client
 
 class Room:
-    def __init__(self, code):
-        self.room_code = code
-        self.fileDict = {}
-        self.fileSend = {}
-        self.users = []
-        #self.files = []
-        """
-    def listFiles(self):
-    	newlst = []
-    	for f in self.fileDict:
-    		newlst.append(self.fileDict[f])
-    	return newlst
-        """
+	def __init__(self, code):
+		self.room_code = code
+		self.fileDict = {}
+		self.fileSend = {}
+		self.users = []
+		#self.files = []
+		"""
+		def listFiles(self):
+			newlst = []
+			for f in self.fileDict:
+				newlst.append(self.fileDict[f])
+			return newlst
+		"""
 
-    def deleteFile(self, key):
-    	if key in self.fileDict:
-    		del self.fileDict[key]
-    	#self.files = listFiles()
+	def deleteFile(self, key):
+		if key in self.fileDict:
+			del self.fileDict[key]
+    		#self.files = listFiles()
 
-    def addFile(self, filename, sender):
-    	self.fileDict[filename] = sender
-        print('before')
-        self.fileSend[filename] = []
-        print('after')
-    	#self.files = listFiles()
+	def addFile(self, filename, sender):
+		self.fileDict[filename] = sender
+		print('before')
+		self.fileSend[filename] = []
+		print('after')
+		#self.files = listFiles()
 
-    def addUser(self, user):
-    	self.users.append(user)
+	def addUser(self, user):
+		self.users.append(user)
 
-    def sendTo(self, filename, receivers):
-        for afile in self.fileSend:
-            if afile == filename:
-                for receiver in receivers:
-                    self.fileSend.setdefault(afile, []).append(receiver)
+	def sendTo(self, filename, receivers):
+		for afile in self.fileSend:
+			if afile == filename:
+				for receiver in receivers:
+					self.fileSend.setdefault(afile, []).append(receiver)
